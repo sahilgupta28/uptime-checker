f20f5cd278bc83dcc81d9a69c30ca98b187a094f
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-06-07 22:37:01 +0530

    use vue js

---
+++ resources/js/components/WebsiteList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div :class="{'red-error' : error}">123</div>
+    <button type="button" @click="error = !erroe">Error</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WebsietList",
+  data: function() {
+    return {
+      error: false
+    };
+  }
+};
+</script>
+
+<style>
+</style>   
