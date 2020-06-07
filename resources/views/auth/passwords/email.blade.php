dc8be53bcdece5b9680951ff6fd0c4e6f36dcbc1
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-06-07 22:33:38 +0530

    Reskin auth with tailwind

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="mx-auto h-full justify-center items-center flex bg-gray-300">
+    <div class="w-96 bg-blue-900 rounded-lg shadow-xl p-6">
+        <h1 class="text-white text-3xl pt-8">{{ env('APP_NAME') }}</h1>
+        <h2 class="text-blue-300">Don't worry. Resetting your password is easy, just tell us the email address you registered with us.</h2>
+        <form method="POST" action="{{ route('password.email') }}" class="pt-8">
+            @csrf
+            <div class="relative">
+                <label for="email" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">{{ __('E-Mail Address') }}</label>
+                <div>
+                    <input id="email" type="email" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-100 outline-none focus:bg-blue-700" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="your@email.com">
+                    @error('email')
+                        <span class="text-red-500 text-sm" role="alert">{{ $message }}</span>
+                    @enderror
                 </div>
             </div>
-        </div>
+
+            <div class="pt-8">
+                <button type="submit" class="uppercase rounded text-gray-100 w-full bg-gray-400 py-2 px-3 text-left text-blue-800">{{ __('Send Password Reset Link') }}</button>
+            </div>
+
+            <div class="flex justify-between pt-8 text-white text-sm font-bold">
+                <a class="" href="{{ route('register') }}">{{ __('Register') }}</a>
+                <a class="" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </div>
+
+        </form>
     </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+<div class="mx-auto h-full justify-center items-center flex bg-gray-300">
+    <div class="w-96 bg-blue-900 rounded-lg shadow-xl p-6">
+        <h1 class="text-white text-3xl pt-8">{{ env('APP_NAME') }}</h1>
+        <h2 class="text-blue-300">Enter your details to cerate a free account.</h2>
+         <form method="POST" action="{{ route('register') }}" class="pt-8">
+            @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="relative">
+                <label for="name" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">{{ __('Name') }}</label>
+                <div>
+                    <input id="name" type="text" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-100 outline-none focus:bg-blue-700" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="John Doe">
+                    @error('name')
+                        <span class="text-red-500 text-sm" role="alert">{{ $message }}</span>
+                    @enderror
                 </div>
             </div>
-        </div>
+
+            <div class="relative pt-3">
+                <label for="email" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">{{ __('E-Mail Address') }}</label>
+                <div>
+                    <input id="email" type="email" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-100 outline-none focus:bg-blue-700" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="your@email.com">
+                    @error('email')
+                        <span class="text-red-500 text-sm" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="relative pt-3">
+                <label for="password" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">{{ __('Password') }}</label>
+                <div>
+                    <input id="password" type="password" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-100 outline-none focus:bg-blue-700" name="password" required autocomplete="new-password" placeholder="secrate">
+                    @error('password')
+                        <span class="text-red-500 text-sm" role="alert">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="relative pt-3">
+                <label for="password-confirm" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">{{ __('Confirm Password') }}</label>
+                <div>
+                    <input id="password-confirm" type="password" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-100 outline-none focus:bg-blue-700" name="password_confirmation" required autocomplete="new-password" placeholder="secrate">
+                </div>
+            </div>
+            <div class="pt-8">
+                <button type="submit" class="uppercase rounded text-gray-100 w-full bg-gray-400 py-2 px-3 text-left text-blue-800">{{ __('Register') }}</button>
+            </div>
+
+            <div class="flex pt-8 justify-end text-white text-sm font-bold">
+                <a href="{{ route('login') }}">Already a member?</a>
+            </div>
+        </form>
     </div>
 </div>
+
 @endsection
