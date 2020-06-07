a210ce1e4c799c29aeaf83dcda34f53f8ab56569
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-06-07 22:52:35 +0530

    Fix style and typo

--- resources/views/dashboard/form.blade.php
+++ resources/views/dashboard/form.blade.php
@@ -8,7 +8,7 @@
             <label for="title" class="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2" for="grid-first-name">
                 Title
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Google" id="title" type="text"  name="title"  autocomplete="current-title" value="{{ old('title') }}">
+            <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Eg: Google" id="title" type="text"  name="title"  autocomplete="current-title" value="{{ old('title') }}">
             @error('title')<p class="text-red-500 text-xs italic">{{ $message }}</p>@enderror
             </div>
 
@@ -16,7 +16,7 @@
             <label for="domain" class="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2" for="grid-last-name">
                 Domain
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" id="domain" type="text" class="form-control @error('domain') is-invalid @enderror" name="domain" value="{{ old('domain') }}" autocomplete="domain" autofocus placeholder="www.google.com">
+            <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" id="domain" type="text" class="form-control @error('domain') is-invalid @enderror" name="domain" value="{{ old('domain') }}" autocomplete="domain" autofocus placeholder="Eg: www.google.com">
             @error('domain')<p class="text-red-500 text-xs italic">{{ $message }}</p>@enderror
             </div>
 
@@ -24,7 +24,7 @@
             <label for="description" class="block uppercase tracking-wide text-gray-500 text-xs font-bold mb-2" for="grid-last-name">
                 Description
             </label>
-            <textarea id="Description" type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="description" rows="1" placeholder="Say Soemthing...">{{ old('description') }}</textarea>
+            <textarea id="Description" type="text" class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="description" rows="1" placeholder="Write a short note...">{{ old('description') }}</textarea>
             @error('description')<p class="text-red-500 text-xs italic">{{ $message }}</p>@enderror
             </div>
 
