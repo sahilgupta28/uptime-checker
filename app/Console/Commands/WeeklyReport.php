87346f779a7411a07069bfaa92ebd1d2d3b14ae9
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-09-23 11:01:51 +0530

    remove comments

--- app/Console/Commands/WeeklyReport.php
+++ app/Console/Commands/WeeklyReport.php
@@ -9,36 +9,14 @@ use Illuminate\Support\Facades\Mail;
 
 class WeeklyReport extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'report:generate';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = "It'll generate weekly reports";
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct(UserInterface $user)
     {
         parent::__construct();
         $this->user = $user;
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
         $users = $this->user->all();
