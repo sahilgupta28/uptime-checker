2a94067ff237bf0e15547d1c4d91d1b23165fda2
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-10-14 10:48:57 +0530

    Add privacy policy at footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,11 +5,17 @@
             <a href="{{ config('constants.FOUNDER.WEBSITE') }}" target="_blank">{{ config('constants.FOUNDER.NAME') }}</a>
         </span>
     </div>
-    <div class="font-semibold text-sm tracking-tight text-blue-300">
+    <div class="w-full flex flex-wrap -mx-3">
+    <div class="font-semibold text-sm tracking-tight text-blue-300 w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <a class="px-1" href="{{ route('home') }}">Home</a> | 
+        <a class="px-1" href="{{ route('privacy.policy') }}" target="_blank">Privacy Policy</a>
+    </div>
+    <div class="font-semibold text-sm tracking-tight text-blue-300 w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <a class="px-1" href="{{ config('constants.FOUNDER.TWITTER') }}" target="_blank"><i class="fa fa-twitter"></i></a>
         <a class="px-1" href="{{ config('constants.FOUNDER.LINKED_IN') }}" target="_blank"><i class="fa fa-linkedin"></i></a>
         <a class="px-1" href="{{ config('constants.FOUNDER.GITHUB') }}" target="_blank"><i class="fa fa-github-alt"></i></a>
         <a class="px-1" href="{{ config('constants.FOUNDER.STACK_OVERFLOW') }}" target="_blank"><i class="fa fa-stack-overflow"></i></a>
         <a class="px-1" href="{{ config('constants.FOUNDER.BLOG') }}" target="_blank"><i class="fab fa-blogger-b"></i></a>
+    </div>
     </div>
 </footer>
