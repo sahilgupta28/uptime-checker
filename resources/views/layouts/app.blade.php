244906f77170e413a7354529683f37df0b37fe29
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-10-20 18:01:24 +0530

    Add icon to home screen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('images/icon.ico') }}" type="image/x-icon" sizes="16x16">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-
 
     <title>{{ config('app.name', 'Uptime') }}</title>
 
