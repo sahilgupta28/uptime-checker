6f83cc725e1255092f952ef492acb17e79f19981
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-12-04 16:16:20 +0530

    Remove comments & 600 seconds frequency

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,39 +7,19 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
     protected $commands = [
-        //
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('test:run')
-            ->everyTenMinutes();
-        $schedule->command('test:run --fail')
             ->everyMinute();
-
         $schedule->command('report:generate')
             ->weeklyOn(7, config('constants.NIGHT_SCHEDULER_TIME'));
         $schedule->command('report:daily')
             ->dailyAt(config('constants.NIGHT_SCHEDULER_TIME'));
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__ . '/Commands');
