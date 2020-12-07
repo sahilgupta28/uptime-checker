931dfcf0cc9eded21205c0c3322faabfd71cbcc4
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-12-07 10:50:58 +0530

    Remove comments

--- app/Console/Commands/SaveTestLog.php
+++ app/Console/Commands/SaveTestLog.php
@@ -7,36 +7,16 @@ use App\Repositories\TestLog\TestLogInterface;
 
 class SaveTestLog extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'test_log:create {data} --queue';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Create a test log.';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct(TestLogInterface $test_log)
     {
         $this->test_log = $test_log;
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
         $data = $this->argument('data');
