29e26664de84046e807080ffaf33e65b4e78400f
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-12-16 14:27:45 +0530

    Remove Auth

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -3,13 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Auth;
 
 class IsAdmin
 {
     public function handle($request, Closure $next)
     {
-        if (Auth::User()->role == config('constants.ROLE.ADMIN')) {
+        if (auth()->User()->role == config('constants.ROLE.ADMIN')) {
             return $next($request);
         }
         return abort('403', 'Not Authorized');
