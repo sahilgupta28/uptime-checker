5caefea14bd07c187a45b25518cd64e7f6fc8596
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-12-16 14:26:31 +0530

    Manage redirects after login & register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,31 +7,18 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    protected function redirectTo()
+    {
+        if (auth()->User()->role == config('constants.ROLE.ADMIN')) {
+            return route('admin.dashboard');
+        }
+        return route('home');
+    }
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
