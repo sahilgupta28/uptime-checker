4ff3fc1d7d2e91c736588002b44e33bbc35c0a37
Sahil Gupta <sahilgupta@ucreate.co.in>
2020-12-17 13:43:54 +0530

    Make group routes for admin & user

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,22 @@ Route::get('login/github', 'SocialLoginController@redirectToProvider')->name('gi
 Route::get('login/github/callback', 'SocialLoginController@handleProviderCallback');
 
 Route::group(['middleware' => ['auth', 'user']], function () {
-    Route::get('/home', 'WebsiteController@index')->name('home');
+    Route::get('/home', 'WebsiteController@list')->name('home');
     Route::post('/website', 'WebsiteController@save')->name('website.create');
     Route::get('/website/{id}', 'WebsiteController@show')->name('website.show');
     Route::put('/website/{id}', 'WebsiteController@update')->name('website.update');
-    Route::patch('/website/{id}', 'WebsiteController@status')->name('website.status');
     Route::delete('/website/{id}', 'WebsiteController@destroy')->name('website.delete');
 
-    Route::post('/website-test/{id}', 'WebsiteController@test')->name('website.test');
     Route::get('/profile/{user_id}', 'UserController@show')->name('user.show');
     Route::put('/profile/{user_id}', 'UserController@update')->name('user.update');
 });
-
+Route::group(['middleware' => ['auth']], function () {
+    Route::patch('/website/{id}', 'WebsiteController@status')->name('website.status');
+    Route::post('/website-test/{id}', 'WebsiteController@test')->name('website.test');
+});
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('dashboard', 'AdminController@dashboard')->name('dashboard');
+    Route::get('user/{user_id}/websites', 'WebsiteController@list')->name('user.websites');
 });
 Route::get('/scheduler/run', 'SchedulerController@run')->name('test.fail');
 Route::get('/privacy-policy', function () {
